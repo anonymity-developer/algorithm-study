0e09c26bd0a8cfc1b3dde02ab7a8df9d8355c56f
anonymity-developer <bjiwon59@gmail.com>
2025-03-18 22:38:08 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,19 @@
+# 완전탐색  |  난이도: 하 
+# 2309 일곱 난쟁이
+
+height = []
+for _ in range(9):
+    height.append(int(input()))
+sum_height = sum(height)
+
+for i in range(9):
+    for j in range(i+1, 9):
+        if sum_height - height[i] - height[j] == 100:
+            del height[j]
+            del height[i]
+            height.sort()
+            print('\n'.join(map(str, height)))
+            break
+    if len(height) == 7: break
+    
+# sys.exit는 코드 진행 자체가 완전 종료되므로, 일반적으로 braek나 flag를 사용

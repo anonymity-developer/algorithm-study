15aa9832b695a0ac66eacd5cfcc3929eb2bc8969
anonymity-developer <bjiwon59@gmail.com>
2025-03-19 11:17:48 +0900

    Update 일곱 난쟁이.py

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -6,7 +6,7 @@ for _ in range(9):
     height.append(int(input()))
 sum_height = sum(height)
 
-for i in range(9):
+for i in range(8):
     for j in range(i+1, 9):
         if sum_height - height[i] - height[j] == 100:
             del height[j]

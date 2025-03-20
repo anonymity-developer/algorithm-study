3ab5840123f26531a8f2c97e5218536bc9c0bcaf
anonymity-developer <bjiwon59@gmail.com>
2025-03-20 17:33:55 +0900

    [Silver II] Title: 부분수열의 합, Time: 204 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,11 +1,13 @@
+# 1182 부분수열의 합
+
 def Csum(num, sequence, goal_sum, index, current_sum, cnt):
-    if index == num:
+    if index == num: # 발견하면 cnt+1, 아니면 cnt 반환
         return cnt+1 if current_sum == goal_sum else cnt
     
-    cnt = Csum(num, sequence, goal_sum, index + 1, current_sum + sequence[index], cnt)
-    cnt = Csum(num, sequence, goal_sum, index + 1, current_sum, cnt)
+    cnt = Csum(num, sequence, goal_sum, index + 1, current_sum + sequence[index], cnt) # 현재 원소 포함
+    cnt = Csum(num, sequence, goal_sum, index + 1, current_sum, cnt) # 현재 원소 미포함
     return cnt
    
-   
-N, S = map(int, input().split()) 
-print(Csum(N, list(map(int, input().split())), S, 0, 0, 0) - (1 if S==0 else 0))
+N, S = map(int, input().split())
+A = list(map(int, input().split()))
+print(Csum(N, A, S, 0, 0, 0) - (1 if S==0 else 0)) # 공집합을 고려한 계산

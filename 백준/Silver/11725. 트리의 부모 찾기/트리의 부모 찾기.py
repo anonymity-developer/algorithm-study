a82051113fe5d2a85ccebd5f7eeecafa207c65e6
anonymity-developer <bjiwon59@gmail.com>
2025-04-01 21:22:49 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 368 ms, Memory: 64432 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,15 +1,15 @@
 # DFS  |  난이도: 중
 # 11725 트리의 부모 찾기
 
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 graph = {i : [] for i in range(1, 1+N)}
-try:
-    while True:
-        node1, node2 = map(int, input().split())
-        graph[node1].append(node2)
-        graph[node2].append(node1)
-except EOFError:
-    pass
+for _ in range(N - 1):
+    node1, node2 = map(int, input().strip().split())
+    graph[node1].append(node2)
+    graph[node2].append(node1)
 
 # 반복문 버전
 def dfs(graph, start):
@@ -40,3 +40,21 @@ for i in answer[2:]:
 # dfs(1, 0)
 # for i in range(2, N + 1):
 #     print(parent[i])
+
+# 다른 사람 코드
+# import sys
+# input = sys.stdin.readline
+# N = int(input())
+# P = [-1] * (N + 1)
+# child = [list() for i in range(N + 1)]
+# for i in range(N - 1):
+#     u, v = map(int, input().split())
+#     child[u].append(v)
+#     child[v].append(u)
+# Q = [1]
+# for i in Q:
+#     for c in child[i]:
+#         if P[i] == c: continue
+#         P[c] = i
+#         Q.append(c)
+# print(*P[2:], sep = '\n')

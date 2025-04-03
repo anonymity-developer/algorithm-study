029de5ffcfd5ad9eacb6fbaafcfeb62b45207f9b
anonymity-developer <bjiwon59@gmail.com>
2025-04-03 21:05:21 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -2,13 +2,21 @@
 # 2748 피보나치 수 2
 
 memo = {}
-
-def fib(n):
+def fib_top_down(n):
     if n <= 1:
         return n
     if n in memo:
         return memo[n]
-    memo[n] = fib(n - 1) + fib(n - 2)
+    memo[n] = fib_top_down(n - 1) + fib_top_down(n - 2)
     return memo[n]
 
-print(fib(int(input())))
+def fib_bottom_up(n):
+    if n == 0:
+        return 0
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[n]
+
+print(fib_bottom_up(int(input())))

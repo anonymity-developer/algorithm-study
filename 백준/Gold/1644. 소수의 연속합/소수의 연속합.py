c8c95484758a85597e57cd834744158d46535483
anonymity-developer <bjiwon59@gmail.com>
2025-11-12 21:46:44 +0900

    [Gold III] Title: 소수의 연속합, Time: 236 ms, Memory: 154284 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -3,41 +3,31 @@
 N = int(input())
 answer = 0
 
-# 예외 처리: 2 미만이면 만들 수 있는 연속 소수 합 없음
 if N < 2:
-    print(0)
+    print(answer)
     exit()
 
-# 에라토스테네스의 체 (i*i부터 지우기)
-temp_list = [i for i in range(N + 1)]
-temp_list[0] = 0
+# 에라토스테네스의 체로 소수 구하기
+temp_list = [i for i in range(N+1)]
 temp_list[1] = 0
-
-limit = int(N ** 0.5)
-for i in range(2, limit + 1):
+for i in range(2, N//2 + 1):
     if temp_list[i] != 0:
-        start = i * i
-        step = i
-        for m in range(start, N + 1, step):
-            temp_list[m] = 0
+        multiple_cnt = 2
+        while temp_list[i] * multiple_cnt <= N:
+            temp_list[i * multiple_cnt] = 0
+            multiple_cnt += 1
+prime_list = list(filter(lambda n : n > 0, temp_list))
 
-prime_list = [v for v in temp_list if v > 0]
-
-# 두 포인터로 연속합 세기 (오른쪽 늘리고, N 이상이면 왼쪽 줄이기)
-left = 0
-right = 0
-temp_sum = 0
-
-while True:
-    if temp_sum >= N:
-        if temp_sum == N:
-            answer += 1
-        temp_sum -= prime_list[left]
-        left += 1
-    else:
-        if right == len(prime_list):
+# 밀어가면서 더해보기
+for j in range(len(prime_list)):
+    temp_sum = 0
+    idx_cnt = 0
+    while j + idx_cnt < len(prime_list):
+        temp_sum += prime_list[j + idx_cnt]
+        if temp_sum >= N:
+            if temp_sum == N:
+                answer += 1
             break
-        temp_sum += prime_list[right]
-        right += 1
-
+        idx_cnt += 1
+        
 print(answer)

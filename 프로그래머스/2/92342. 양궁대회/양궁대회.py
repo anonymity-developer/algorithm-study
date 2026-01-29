5f16b0c4d5741fbaedc94ebfdf66bda328c948e1
anonymity-developer <bjiwon59@gmail.com>
2026-01-29 19:22:49 +0900

    [level 2] Title: 양궁대회, Time: 2.45 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -0,0 +1,44 @@
+def solution(n, info):
+    def gap(ryan):
+        s = 0
+        for i in range(11):
+            if ryan[i] == 0 and info[i] == 0:
+                continue
+            if ryan[i] > info[i]:
+                s += 10 - i
+            else:
+                s -= 10 - i
+        return s
+
+    def better(a, b):
+        # 동점이면 더 낮은 점수(인덱스 큰 쪽)에 많이 쏜 배열 우선
+        for i in range(10, -1, -1):
+            if a[i] != b[i]:
+                return a[i] > b[i]
+        return False
+
+    best = None
+    best_gap = 0
+
+    def dfs(idx, left, ryan):
+        nonlocal best, best_gap
+
+        if idx == -1:
+            ryan[10] += left
+            g = gap(ryan)
+            if g > 0 and (g > best_gap or (g == best_gap and (best is None or better(ryan, best)))):
+                best_gap = g
+                best = ryan[:]
+            ryan[10] -= left
+            return
+
+        need = info[idx] + 1
+        if left >= need:
+            ryan[idx] = need
+            dfs(idx - 1, left - need, ryan)
+            ryan[idx] = 0
+
+        dfs(idx - 1, left, ryan)
+
+    dfs(10, n, [0] * 11)
+    return best if best is not None else [-1]
